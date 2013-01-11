73010ca5c03de32d53696907b72b2d8e100bb0db
Willem C. van Daalen <wvandaalen@outlook.com>
2013-01-11 17:17:23 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Muse::Application.config.secret_token = 'e2e08bb36ce8be7f92e782edd6ceb89264c12c7d8c88d3749c905223441c53ad9e14189eb2d552f248d8fc046f71cf646e0a496e9a4b72ec3ee61a2781fbc2ef'
