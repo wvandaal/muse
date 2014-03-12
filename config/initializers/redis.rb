7030ca648098b9e88622f63bac853aea1560cbc1
Willem C. van Daalen <wvandaalen@outlook.com>
2014-03-12 12:54:32 -0400

    Specifies Sidekiq.redis in redis.rb config file

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,3 @@
-if ENV["REDISTOGO_URL"]
-    uri = URI.parse(ENV["REDISTOGO_URL"])
-    REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-end
+uri = URI.parse(ENV["REDISTOGO_URL"])
+REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+Sidekiq.redis = REDIS
