61170f703f1b54e2763a09ebcb000cb085ed9ce8
Willem C. van Daalen <wvandaalen@outlook.com>
2014-03-12 13:02:45 -0400

    Removes Sidekiq.redis assignment from redis.rb and adds url: to sidekiq.rb

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,9 @@
 require 'sidekiq'
 
 Sidekiq.configure_client do |config|
-  config.redis = { :size => 1 }
+  config.redis = { :size => 1, url: "redis://redistogo:cdad24f17dfea1c0fe8b4d7b9a95b1cf@albacore.redistogo.com:9562/" }
 end
 
 Sidekiq.configure_server do |config|
-  config.redis = { :size => 2 }
+  config.redis = { :size => 2, url: "redis://redistogo:cdad24f17dfea1c0fe8b4d7b9a95b1cf@albacore.redistogo.com:9562/"}
 end
