cfda091791926d1872f4eb3751c7ff7041ccefd2
Willem C. van Daalen <wvandaalen@outlook.com>
2014-03-12 15:42:39 -0400

    changes recommendable config.redis settings in config/initializers/recommendable.rb

--- config/initializers/recommendable.rb
+++ config/initializers/recommendable.rb
@@ -4,7 +4,8 @@ Recommendable.configure do |config|
   # Recommendable's connection to Redis.
   #
   # Default: localhost:6379/0
-  config.redis = Redis.new(:host => 'localhost', :port => 6379, :db => 0)
+  # config.redis = Redis.new(:host => 'localhost', :port => 6379, :db => 0)
+  config.redis = REDIS
 
   # A prefix for all keys Recommendable uses.
   #
